eca1c68609e2258273dab597c9d0e2c3c291083e
glenntam <glenntam@gmail.com>
2025-04-17 03:17:44 +0800

    Before bangkok flight

--- core/ib_client.py
+++ core/ib_client.py
@@ -1,6 +1,6 @@
 import os
 
-from ib_async import Future, IB, util
+from ib_async import Contract, Future, IB, util
 from core.event_manager import event_manager
 
 
@@ -8,29 +8,85 @@ util.patchAsyncio()
 ib = IB()
 ib.connect(os.getenv('IB_HOST'), os.getenv('IB_PORT'), clientId=0)
 
+state = {}
 
-mes = Future('MES', '20250620')
-mnq = Future('MNQ', '20250620')
+# Contracts and Tickers
+state['assets'] = ['MESM5', 'MNQM5']
+contracts = {}
+for asset in state['assets']:
+    if asset.startswith(('MES', 'ES', 'MNQ', 'NQ', 'MYM', 'YM', 'M2K', 'RTY', 'CL', 'GC')):
+        contract = Future(localSymbol=asset, exchange='CME', currency='USD')
+        qualified = ib.qualifyContracts(contract)
+        contracts[asset] = qualified[0]
+    else:
+        raise Exception("Qualify asset class manually:")
+        # Fill manually
+        #contract = Stock(localSymbol=asset, exchange='', currency='USD')
+    qualified = ib.qualifyContracts(contract)
+    contracts[asset] = qualified[0]
 
-contracts = {'MNQ': ib.qualifyContracts(mnq)[0],
-             'MES': ib.qualifyContracts(mes)[0],
-            }
+tickers = {}
+for c in contracts:
+    ib.reqMktData(contracts[c], '233', False, False, [])
+    # https://ib-insync.readthedocs.io/api.html#ib_insync.ib.IB.reqMktData
+    tickers[c] = ib.ticker(contracts[c])
+ib.sleep(1.0)
 
-# tickers = {}
-# for c in contracts:
-#     ib.reqMktData(contracts[c], '233', False, False, [])
-#     # https://ib-insync.readthedocs.io/api.html#ib_insync.ib.IB.reqMktData
-#     tickers[c] = ib.ticker(contracts[c])
-#     ib.sleep(1.0)
+# Positions
+
+def update_state():
+    try:
+        ib.sleep(0.1)
+        state['system_time'] = ib.reqCurrentTime()
+        state['account_summary'] = ib.accountSummary()
+        state['mes_last'] = tickers['MESM5'].last
+        state['mnq_last'] = tickers['MNQM5'].last
+        state['portfolio'] = ib.portfolio()
+        state['positions'] = ib.positions()
+        state['debug'] = populate_pos_data()
+        #populate_pos_data()
+    except RuntimeError as e:
+        if "Event loop stopped before Future completed" in str(e):
+            pass  # Event loop is already stopping, ignore this error.
+        else:
+            raise  # For any other runtime error, re-raise.
+    except ConnectionError as e:
+        if "Not connected" in str(e):
+            pass  # Keep retrying anyway
+        else:
+            raise
+
+ib.pendingTickersEvent += update_state
+
+def populate_pos_data():
+    pos = ib.portfolio()
+    data = []
+    current_owned = []
+    for p in pos:
+        row = []
+        row.append(p.contract.localSymbol)
+        current_owned.append(p.contract.localSymbol)
+        row.append(str(p.position))
+        row.append(str(round((p.averageCost / float(multiplier)), 2)))
+        row.append(str(round(p.marketPrice, 2)))
+        row.append(str(int(round(p.unrealizedPNL, 0))))
+        row.append(str(int(round(p.realizedPNL, 0))))
+        data.append(row)
+    for asset in state['assets']:
+        if asset not in current_owned:
+            data.append([asset, '0', '0.00', 'FIXME', '0', '0'])
+
+    # sort alphabetically, except the header
+    sorted_rows = sorted(data, key=lambda sublist: sublist[0])
+    pos_data = [['localSymbol', 'position', 'averageCost', 'marketPrice', 'unrealizedPNL', 'realizedPNL']]
+    for each_row in sorted_rows:
+        pos_data.append(each_row)
+    return pos_data
 
 def graceful_shutdown():
-    for c in contracts:
-        ib.cancelMktData(contracts[c])
-    ib.sleep(0.2)
-    ib.disconnect()
-
-#from pprint import pprint
-#pprint(tickers['MES'].last)
-#ib.disconnect()
-#input("PAUSE")
+        ib.pendingTickersEvent -= update_state
+        for c in contracts:
+            ib.cancelMktData(contracts[c])
+        ib.sleep(0.5)
+        ib.disconnect()
 
--- main.py
+++ main.py
@@ -1,97 +1,42 @@
 import os
 import sys
-from collections import defaultdict
 from zoneinfo import ZoneInfo
 
 import urwid
 from dotenv import load_dotenv; load_dotenv()
 
-from core.event_manager import event_manager
-from core.ib_client import ib, util, graceful_shutdown
+#from core.event_manager import event_manager
+from core.ib_client import ib, util, state, update_state, graceful_shutdown
 from core.logger import logger, log
 
 
 class TUI:
     def __init__(self):
+        ib.errorEvent += logger.OnIBErrorEvent  # Catch IB TWS errors
         log.info("HYDRA started.")
         self.paused = False
         self.console_messages = []
         self.bots = []
-        #self.current_bot = 0
 
-    def start(self):
-        """Link ib_async event loop to urwid's MainLoop."""
-
-        # ib_async's built-in asyncio event loop
-        self.ib_loop = util.getLoop()
-
-        # Tell urwid the ib loop is an asyncio loop
-        self.my_asyncio_loop = urwid.AsyncioEventLoop(loop=self.ib_loop)
-
-        # Init bots
-        #self.initialize_bots()
-
-        self.draw_layout()
-
-        # Tell urwid to use the ib asyncio loop as its main loop
-        self.loop = urwid.MainLoop(
-            self.frame,
-            palette=self.palette,
-            unhandled_input=self.handle_input,
-            event_loop=self.my_asyncio_loop
-        )
-
-        #ib.pendingTickersEvent += self.on_pending_tickers
-        ib.errorEvent += logger.OnIBErrorEvent  # Catch IB TWS errors
-
-        # Schedule the first refresh immediately
-        self.loop.set_alarm_in(0, self.refresh_display)
-        self.loop.run()
-
-#     def on_pending_tickers(self, tickers):
-#         for t in tickers:
-#             if t == 'MES':
-#                 self.middle_left_ticker.original_text.set_text(str(tickers[t].last))
-#             if t == 'MNQ':
-#                 self.middle_left_df.original_text.set_text(str(tickers[t].last))
-
-    def initialize_bots(self):
-        self.bots = {0: 'master'}
-#         self.bots = {
-#                 0: Bot('master', self.bot_callback, client_id=0),
-#                 1: Bot('ss_live', self.bot_callback, client_id=1),
-#         }
-#         for bot in self.bots:
-#             self.bots[bot].connect()
-
-
-    def bot_callback(self, client_id, widget, s):
-        if widget == 'time':
-            try:
-                self.top_text.base_widget.set_text(s)
-            except:
-                log.error(f"Error updating time: {e}")
-                self.top_text.base_widget.set_text("Error retrieving time.")
-        if widget == 'ticker':
-            try:
-                self.middle_left_ticker.base_widget.set_text(s)
-            except:
-                pass
-        if widget == 'bars':
-            try:
-                self.middle_left_df.base_widget.set_text(s)
-            except:
-                pass
-
-    def draw_layout(self):
-        # UI
+    def draw_initial_layout(self):
+        """Draw the initial boxes and text fields for the frame."""
+        # Color scheme
         self.palette = [
             ('normal', 'white', 'black'),   # Default text colour
             ('focus', 'black', 'dark red', 'standout'),  # Highlighted widget when focused
         ]
 
-        self.top_text = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
-        #self.dropdown = [bot.codename for bot in self.bots.values()]
+        # Boxes and Text fields
+        self.top_time = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+        self.top_mes = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+        self.top_mnq = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+        self.top_net_liquidation = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+
+        self.pos_local_symbol = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+        self.pos_position = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+        self.pos_average_cost = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+        self.pos_cost = urwid.AttrMap(urwid.Text("", wrap='clip'), None, 'focus')
+        #
         self.dropdown = ['0', '1', '2']
         self.dropdown_btns = []
         self.selected = [self.dropdown[0]]
@@ -103,14 +48,21 @@ class TUI:
                 log.debug(f'Selected: #{self.current_bot} "{self.selected[0]}"')
             urwid.connect_signal(btn, 'click', on_click)
             self.dropdown_btns.append(btn)
-
         self.top_dropdown = urwid.ListBox(urwid.SimpleFocusListWalker(self.dropdown_btns))
-        self.top = urwid.LineBox(urwid.Filler(urwid.Columns([('weight', 1, self.top_text), ('weight', 1, self.top_dropdown)], box_columns=[1])))
+
+        self.top = urwid.LineBox(urwid.Filler(urwid.Columns([
+                                                                ('weight', 8, self.top_time),
+                                                                ('weight', 4, self.top_mes),
+                                                                ('weight', 4, self.top_mnq),
+                                                                ('weight', 4, self.top_net_liquidation),
+                                                                ('weight', 1, self.top_dropdown)
+                                                            ], box_columns=[4])))
 
         self.middle_left_input = urwid.AttrMap(urwid.Edit("Input: ", wrap='clip'), None, 'focus')
         self.middle_left_ticker = urwid.AttrMap(urwid.Text("ML", wrap='clip'), None, 'focus')
-        self.middle_left_df = urwid.AttrMap(urwid.Text("ML", wrap='clip'), None, 'focus')
-        self.middle_left_pile = urwid.Pile([self.middle_left_input, self.middle_left_ticker, self.middle_left_df])
+        self.debug = urwid.AttrMap(urwid.Text(""), 'normal', 'focus')
+        self.debug2 = urwid.AttrMap(urwid.Text(""), 'normal', 'focus')
+        self.middle_left_pile = urwid.Pile([self.middle_left_input, self.middle_left_ticker, self.debug, self.debug2])
         self.middle_left = urwid.LineBox(self.middle_left_pile)
 
         self.middle_right_text = urwid.AttrMap(urwid.Text("MR", wrap='clip'), None, 'focus')
@@ -131,61 +83,97 @@ class TUI:
             (9, self.bottom),           # Fixed height for the bottom section
         ])
 
+    def bind_async_loop(self):
+        """Link ib_async event loop to urwid's MainLoop."""
+        # ib_async's built-in asyncio event loop
+        self.ib_loop = util.getLoop()
+        # Tell urwid the ib loop is an asyncio loop
+        self.my_asyncio_loop = urwid.AsyncioEventLoop(loop=self.ib_loop)
+        # Init bots
+        #self.initialize_bots()
+        # Tell urwid to use the ib asyncio loop as its main loop
+        self.loop = urwid.MainLoop(
+            self.frame,
+            palette=self.palette,
+            unhandled_input=self.handle_input,
+            event_loop=self.my_asyncio_loop
+        )
+        # Schedule the first refresh when loop runs
+        self.loop.set_alarm_in(0, self.refresh_display)
+
+    def start(self):
+        self.loop.run()
+
+    def refresh_display(self, loop, user_data=None):
+        if not self.paused:
+            update_state()
+
+            self.console_messages = logger.get_console_messages()
+            self.bottom_text.base_widget.set_text("\n".join(self.console_messages))
+
+            ib_time = str(state['system_time'].now(ZoneInfo("America/New_York"))) + "  (New York)"
+            self.top_time.base_widget.set_text(ib_time)
+
+            self.top_mes.base_widget.set_text(str(state['mes_last']))
+            self.top_mnq.base_widget.set_text(str(state['mnq_last']))
+
+            net_liquidity_header = str(state['account_summary'][88].tag + ': ')
+            net_liquidity_value = str('{:,}'.format(int(state['account_summary'][88].value.split('.')[0])))
+            self.top_net_liquidation.base_widget.set_text(net_liquidity_header + net_liquidity_value)
+
+            self.debug.base_widget.set_text(str(state['debug']))
+
+            loop.set_alarm_in(0, self.refresh_display)
+
+    def initialize_bots(self):
+        """Initialize the bots in a dict."""
+        self.bots = {0: 'master'}
+        #self.bots = {
+        #        0: Bot('master', self.bot_callback, client_id=0),
+        #        1: Bot('ss_live', self.bot_callback, client_id=1),
+        #}
+        #for bot in self.bots:
+        #    self.bots[bot].connect()
+
+    def bot_callback(self, client_id, widget, s):
+        if widget == 'time':
+            try:
+                self.top_time.base_widget.set_text(s)
+            except:
+                log.error(f"Error updating time: {e}")
+                self.top_time.base_widget.set_text("Error retrieving time.")
+        if widget == 'ticker':
+            try:
+                self.middle_left_ticker.base_widget.set_text(s)
+            except:
+                pass
+        if widget == 'bars':
+            try:
+                self.middle_left_df.base_widget.set_text(s)
+            except:
+                pass
 
     def get_key_int_from_bot_codename(self, codename):
         """Return integer key number of self.bots given a codename. Return 0 if none found."""
         return next((key for key, bot in self.bots.items() if codename == bot.codename), 0)
 
-    def refresh_display(self, loop, user_data=None):
-        self.console_messages = logger.get_console_messages()
-        self.bottom_text.base_widget.set_text("\n".join(self.console_messages))
-
-        # Delete later. Testing TWS error emit
-        #con = contract.ContFuture('YM', exchange='CBOT')
-        #c = self.ib.qualifyContracts(con)[0]
-        #self.ib.reqMktData(c, genericTickList='', snapshot=False, regulatorySnapshot=False)
-        #ticker = self.ib.ticker(c)
-
-        #ticker
-
-        ib_time_obj = ib.reqCurrentTime()
-        ib_time = str(ib_time_obj.now(ZoneInfo("America/New_York"))) + "  (New York)"
-        self.top_text.base_widget.set_text(ib_time)
-
-
-        #log.debug("beforetime")
-        #self.top_text.original_widget.set_text(str(ib.reqCurrentTime().now(ZoneInfo("America/New_York"))) + "  (New York)")
-        #log.debug("beforesleep")
-        #ib.sleep(0.15)
-
-        #self.bots[0].display_time()
-
-        if not self.paused:
-            ib.sleep(0.05)
-            loop.set_alarm_in(0, self.refresh_display)
-            #self.loop.draw_screen()
-
-
     def handle_input(self, key):
         if isinstance(key, str):
             input_text = self.middle_left_input.base_widget.get_edit_text()
-#             if true:  # fixme: only when edit widget is focused
-#                 if key == "enter":
-#                     contract_input = self.middle_left_input.base_widget.get_edit_text().upper()
+            #if true:  # fixme: only when edit widget is focused
+            #    if key == "enter":
+            #        contract_input = self.middle_left_input.base_widget.get_edit_text().upper()
 
-#                     contract_obj = self.bots[self.current_bot].qualify(contract_input)
-#                     self.bots[self.current_bot].start_ticker(contract_obj)
-#                     self.bots[self.current_bot].start_bars(contract_obj)
-
+            #        contract_obj = self.bots[self.current_bot].qualify(contract_input)
+            #        self.bots[self.current_bot].start_ticker(contract_obj)
+            #        self.bots[self.current_bot].start_bars(contract_obj)
             if key.lower() == "q":
-#                 for bot in self.bots:
-#                     log.debug(f"{bot} stopping")
-#                     self.bots[bot].disconnect()
-#                     util.sleep(10)
-#                     log.debug(f"{bot} stopped")
-#                 log.info("all bots stopped. hydra stopping.")
+                self.paused = True
                 graceful_shutdown()
                 raise urwid.ExitMainLoop()
+            if key.lower() == "esc":
+                self.loop.screen.clear()
+                self.loop.draw_screen()
             elif key.lower() == "p":
                 if self.paused:
                     self.paused = False
@@ -196,7 +184,8 @@ class TUI:
                     self.paused = True
 
 
-
 if __name__ == "__main__":
     app = TUI()
+    app.draw_initial_layout()
+    app.bind_async_loop()
     app.start()
